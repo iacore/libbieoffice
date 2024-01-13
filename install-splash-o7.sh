a874328684c8974eb3b2b8ada754b99da29761f4
iacore <noreply+gpg-stub@1a-insec.net>
2024-01-13 05:56:28 +0000

    Add pacman script

--- install-splash-o7.sh
+++ install-splash-o7.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/fish
+cd (dirname (status -f))
 cp splash/libbie-highres_o7.png /usr/lib/libreoffice/program/intro-highres.png
 cp splash/libbie_o7.png /usr/lib/libreoffice/program/intro.png
